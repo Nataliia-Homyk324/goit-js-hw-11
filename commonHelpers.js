550769ba2f6223e9fb88e3911a0a216359514280
Nataliia-Homyk324 <144448340+Nataliia-Homyk324@users.noreply.github.com>
2024-02-08 17:01:10 +0000

    Deploying to gh-pages from @ Nataliia-Homyk324/goit-js-hw-11@df62a9302db2249932ed7de77bef2bec8294ef2b 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,29 @@
-import{i as a}from"./assets/vendor-ad859c2f.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=n(e);fetch(e.href,r)}})();const l="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAEZSURBVHgBrZVRDoMgDIar0fi6m2xH2Y3Ek2w3cFfZXXwBljFQCai0xemfoCaU/xNpKwAirXWrlDY5w8ViPhVmbgwIO14A3zeQKq/2IuwaqOu6A07+zaVUT8iUi+V28rd5NuSIOQs5wxyFUOZKqRtnmIpZQCZz3acWcgcXUjkF0b2bmwFSpAxskMAgYecaWSsFC8AgnHkMqIBR09TCGrnHsZic5iLs3By3ngWsITMgy9yphEyVJZjUM6esHUS9yfea7N7DAmJz/1niM2EhVBZR2UKl8DS/SFO80OhUnCBkoYWgz2MdNAx8q0jFOK/Fy1GQvdqYe50BQc3PgLDmRyCU+aYOfFsoCtPa+yXvp2/ue9rHqLATflCf5QflVUcNIrzeSwAAAABJRU5ErkJggg==",A=document.querySelector(".form-search");document.querySelector(".gallery");const c=document.querySelector(".loader");c.style.display="none";A.addEventListener("submit",f);function f(t){if(t.preventDefault(),t.currentTarget.elements.search.value===""){a.error({message:"The search query cannot be empty",timeout:5e3,close:!1,position:"topRight",color:"#ef4040",messageSize:16,messageColor:"	#fff",title:"Error",titleSize:16,titleColor:"#fff",iconUrl:l});return}c.style.display="flex",t.currentTarget.elements.search.value="Search images....",t.target.elements.search.value}
+import{i as c,S as g}from"./assets/vendor-5b791d57.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const n="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAEZSURBVHgBrZVRDoMgDIar0fi6m2xH2Y3Ek2w3cFfZXXwBljFQCai0xemfoCaU/xNpKwAirXWrlDY5w8ViPhVmbgwIO14A3zeQKq/2IuwaqOu6A07+zaVUT8iUi+V28rd5NuSIOQs5wxyFUOZKqRtnmIpZQCZz3acWcgcXUjkF0b2bmwFSpAxskMAgYecaWSsFC8AgnHkMqIBR09TCGrnHsZic5iLs3By3ngWsITMgy9yphEyVJZjUM6esHUS9yfea7N7DAmJz/1niM2EhVBZR2UKl8DS/SFO80OhUnCBkoYWgz2MdNAx8q0jFOK/Fy1GQvdqYe50BQc3PgLDmRyCU+aYOfFsoCtPa+yXvp2/ue9rHqLATflCf5QflVUcNIrzeSwAAAABJRU5ErkJggg==",f=document.querySelector(".form-search"),m=document.querySelector(".gallery"),l=document.querySelector(".loader");l.style.display="none";f.addEventListener("submit",A);function A(o){if(o.preventDefault(),o.target.elements.search.value===""){c.error({message:"The search query cannot be empty",timeout:5e3,close:!1,position:"topLeft",backgroundColor:"#ef4040",messageSize:16,messageColor:"#fff",title:"Error",titleSize:16,titleColor:"#fff",iconUrl:n});return}l.style.display="flex";const r=o.target.elements.search.value.trim();h(r).then(s=>{l.style.display="none",s.hits.length||c.show({title:"Sorry,",message:" there are no images matching your search query. Please try again!",position:"topLeft",backgroundColor:"#ef4040",messageSize:16,messageColor:"#fff",titleSize:16,titleColor:"#fff",iconUrl:n}),console.log(s.hits),console.log(s.hits.length),m.insertAdjacentHTML("afterbegin",d(s.hits)),new g(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250}).refresh(),f.reset()}).catch(s=>{l.style.display="none",console.log(s)})}function h(o){const r="https://pixabay.com/api/",s="42245088-70138ac1b5442bea0784205eb",i=new URLSearchParams({key:s,q:o,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${r}?${i}`).then(e=>{if(!e.ok)throw new Error(e.statusText);return e.json()})}function d(o){return o.map(({webformatURL:r,largeImageURL:s,tags:i,likes:e,views:t,comments:a,downloads:u})=>`<li class="gallery-item">
+          <a class="gallery-link" href="${s}">
+            <img
+              class="gallery-image"
+              src="${r}"
+              alt="${i}"
+              width="360"
+            />
+          </a>
+          <div class="thumb-block">
+            <div class="block">
+              <h2 class="tittle">Likes</h2>
+              <p class="amount">${e}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Views</h2>
+              <p class="amount">${t}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Comments</h2>
+              <p class="amount">${a}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Downloads</h2>
+              <p class="amount">${u}</p>
+            </div>
+          </div>
+        </li>`).join("")}
 //# sourceMappingURL=commonHelpers.js.map
